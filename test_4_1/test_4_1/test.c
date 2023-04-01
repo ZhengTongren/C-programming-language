3212aa06beee520fe797583c8cbb6a650c9748df
tianhuo-tongren <1811334612@qq.com>
2023-04-01 20:00:12 +0800

    23_4_1 C语言代码提交

---
+++ test_4_1/test_4_1/test.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int num = 20;
+//	printf("%d\n", num = 10);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n",sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+//int a = 100;
+//int main()
+//{
+//	printf("%d\n", a);
+//	int a = 10;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum=%d", sum);
+//
+//	return 0;
+//}
+
