c278153ce9a2543de38642a7c72e7ffa7925d110
tianhuo-tongren <1811334612@qq.com>
2023-04-07 22:24:31 +0800

    23_4_7 C语言代码提交

---
+++ test_4_7/test_4_7/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	if (n % 5 == 0)
+//		printf("YES\n");
+//	else
+//		printf("NO\n");
+//	return 0;
+//}\
+
