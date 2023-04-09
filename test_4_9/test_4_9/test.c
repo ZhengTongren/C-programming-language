624c36cb940e4127d0d796637bf5f3273418ecf5
tianhuo-tongren <1811334612@qq.com>
2023-04-09 20:17:31 +0800

    后续

--- test_4_9/test_4_9/test.c
+++ test_4_9/test_4_9/test.c
@@ -64,26 +64,93 @@
 //	return 0;
 //}
 
-int main()
-{
-	int n, arr[9] = { 0 }, count = 0;
-	scanf("%d", &n);
-	while (n)
-	{
-		int m = n % 10;
-		if (m % 2)
-			arr[count++] = 1;
-		else
-			arr[count++] = 0;
-		n /= 10;
-	}
-	int result = 0;
-	for (int i = count - 1; i >= 0; i--)
-	{
-		result = result * 10 + arr[i];
-		//printf("%d",arr[i]);
-	}
-	printf("%d", result);
-	return 0;
-}
+//int main()
+//{
+//	int n, arr[9] = { 0 }, count = 0;
+//	scanf("%d", &n);
+//	while (n)
+//	{
+//		int m = n % 10;
+//		if (m % 2)
+//			arr[count++] = 1;
+//		else
+//			arr[count++] = 0;
+//		n /= 10;
+//	}
+//	int result = 0;
+//	for (int i = count - 1; i >= 0; i--)
+//	{
+//		result = result * 10 + arr[i];
+//		//printf("%d",arr[i]);
+//	}
+//	printf("%d", result);
+//	return 0;
+//}
+
+
+//extern int g_val;
+//
+//int main()
+//{
+//	printf("%d", g_val);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	const int a = 3;
+//	//a = 10;
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
+
+//enum color
+//{	
+//	red,
+//	green,
+//	blue
+//
+//};
+//int main()
+//{
+//	printf("%d\n", red);
+//	printf("%d\n", green);
+//	printf("%d\n", blue);
+//	return 0;
+//}
+
+#include<string.h>
+//
+//int main()
+//{
+//	char arr[] = {'a','b','c','d','e','f','\0'};
+//	char arr2[] = "abcdef";
+//
+//	int len1 = strlen(arr);
+//	printf("%d\n", len1);
+//	int len2 = strlen(arr2);
+//	printf("%d\n", len2);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("(are you ok\?\?)");
+//	return 0;
+//}
+
+
+//int main() 
+//{
+//	//printf("\a");
+//	//printf("abc\bdef");
+//
+//	printf("ab\r");
+//	return 0;
+//}
 
