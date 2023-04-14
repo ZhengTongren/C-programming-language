3ad045aa4bb39aeec6a90514ed8d141db45dc97b
tianhuo-tongren <1811334612@qq.com>
2023-04-14 21:35:26 +0800

    23_4_14 C语言代码提交

---
+++ test_4_14/test_4_14/test2.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//    {
+//        int a = 10;
+//    }
+//    printf("%d\n", a);
+//
+//    return 0;
+//}
+
+enum Sex
+{
+	male,
+	female,
+	secret
+};
+
+int main()
+{
+	printf("%d\n", male);
+	printf("%d\n", female);
+	printf("%d\n", secret);
+	return 0;
+}
