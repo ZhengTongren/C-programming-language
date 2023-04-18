277fbad78b919f990de1f3d6c9012849dcbef5d5
tianhuo-tongren <1811334612@qq.com>
2023-04-18 21:58:20 +0800

    23_4_18 C语言代码提交

--- test_4_18/test_4_18/test.c
+++ test_4_18/test_4_18/test.c
@@ -143,3 +143,98 @@
 //}
 
 
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//			printf("%d*%d=%2d ", i, j, i * j);
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int arr[10] = { 9,5,6,10,22,66,82,3,0,60 };
+//	int max = arr[0];
+//	for (int i = 1; i < 10; i++)
+//	{
+//		if (arr[i] > max)
+//			max = arr[i];
+//	}
+//	printf("%d\n", max);
+//
+//	return 0;
+//}
+
+
+//
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 2 == 1)
+//		{
+//			sum += 1.0 / i;
+//		}
+//		else if (i % 2 == 0)
+//		{
+//			sum -= 1.0 / i;
+//		}
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		int s1 = i / 10;
+//		if (s1 == 9)
+//			count++;
+//		int s2 = i % 10;
+//		if (s2 == 9)
+//			count++;
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int n = 7;
+//	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+//
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;
+//	int right = sz - 1;
+//
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] == n)
+//		{
+//			printf("找到了，下标是%d\n", mid);
+//			break;
+//		}
+//		if (arr[mid] > n)
+//			right = mid - 1;
+//		if (arr[mid] < n)
+//			left = mid + 1;
+//
+//	}
+//	
+//	return 0;
+//}
