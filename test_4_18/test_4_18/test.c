4777fc0fabb15098537fd5d8ff55d4c5991dc445
tianhuo-tongren <1811334612@qq.com>
2023-04-18 15:13:59 +0800

    23_4_18 C语言代码提交

---
+++ test_4_18/test_4_18/test.c
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//int main()
+//{
+//	int a = 10;
+//
+//	if (a > 5)
+//		printf("%d", a);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	if (a == 10)
+//	{
+//		printf("%d\n", a);
+//		printf("Hello world!\n");
+//	}
+//	return 0;
+//}
+
+
+//if (a > 5)
+//{
+//	if (a > 7)
+//		printf("%d", a);
+//	else
+//		printf("%d", 5 - a);
+//}
+
+//if (a = 5)
+//{
+//
+//}
+//
+//if (a == 5)
+//{
+//
+//}
+//
+//if (5 == a)
+//{
+//
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	//输入今天星期几，判断是工作日还是周末
+//	switch (n)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("Weekday\n");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("Weekend\n");
+//		break;
+//	default:
+//		printf("wrong!\n");
+//		break;
+//	}
+//	return 0;
+//}
