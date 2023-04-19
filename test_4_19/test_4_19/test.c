6d82d149ee3677a4242d5002e9bbf096f0bd2fa6
tianhuo-tongren <1811334612@qq.com>
2023-04-19 21:00:09 +0800

    23_4_19 C语言代码提交

---
+++ test_4_19/test_4_19/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+#include<Windows.h>
+
+//int main()
+//{
+//	char arr1[] = "Welcome to my world";
+//	char arr2[] = "###################";
+//
+//	int sz = strlen(arr1);
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		printf("\a");
+//		Sleep(1000);
+//		system("cls");
+//
+//		left++;
+//		right--;
+//	}
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
