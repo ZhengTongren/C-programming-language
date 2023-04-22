73a9c9604971b7150ae4b58788d25a029d3696ad
tianhuo-tongren <1811334612@qq.com>
2023-04-22 17:00:16 +0800

    23_4_22 C语言代码提交

---
+++ test_4_22/test_4_22/test.c
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//void change(int* a, int* b)
+//{
+//	int tmp = *a;
+//	*a = *b;
+//	*b = tmp;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//
+//	change(&a, &b);
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
+
+
+//void judge(int y)
+//{
+//	if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+//		printf("%d是闰年\n", y);
+//	else
+//		printf("%d不是闰年\n", y);
+//}
+//int main()
+//{
+//	int y = 0;
+//	scanf("%d", &y);
+//	
+//	judge(y);
+//
+//	return 0;
+//}
+
+#include<math.h>
+//void judge_su(int i)
+//{
+//	int j = 0;
+//	int k = 0;
+//	for (j = 2; j <= sqrt(i); j++)
+//	{
+//		if (i % j == 0)
+//		{
+//			k = 1;
+//			break;
+//		}
+//	}
+//	if (k == 0)
+//		printf("%d ", i);
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//		judge_su(i);
+//
+//	return 0;
+//}
+
+
