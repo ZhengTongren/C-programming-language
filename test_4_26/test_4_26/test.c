db9aacf6fd5d8428d2e7a33807221ce78585ebe4
tianhuo-tongren <1811334612@qq.com>
2023-04-26 23:13:54 +0800

    23_4_26 C语言代码提交

---
+++ test_4_26/test_4_26/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//void test(int* p)
+//{
+//	(*p)++;
+//  // (*p)++ != (*p++) 
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	test(&num);
+//	test(&num);
+//	printf("num=%d\n", num);
+//
+//	return 0;
+//}
+
+//int test(int n)
+//{
+//	return ++n;
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	num=test(num);
+//	num=test(num);
+//	printf("num=%d\n", num);
+//
+//	return 0;
+//}
