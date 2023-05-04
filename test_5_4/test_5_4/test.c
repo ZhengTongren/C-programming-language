e320132f87838dcc5cf9193eb37cf3dfbbcec3ea
tianhuo-tongren <1811334612@qq.com>
2023-05-05 00:02:31 +0800

    23_5_4 C语言代码提交

---
+++ test_5_4/test_5_4/test.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("Practice makes perfect!\n");
+//    return 0;
+//}
+
+//
+//int main()
+//{
+//    printf("v   v\n");
+//    printf(" v v \n");
+//    printf("  v  \n");
+//
+//    return 0;
+//}
+
+//
+//int main()
+//{
+//    printf("The size of short is %d bytes.\n", sizeof(short));
+//    printf("The size of int is %d bytes.\n", sizeof(int));
+//    printf("The size of long is %d bytes.\n", sizeof(long));
+//    printf("The size of long long is %d bytes.\n", sizeof(long long));
+//
+//    return 0;
+//}
+//
+
+//
+//int main()
+//{
+//    printf("     **     \n");
+//    printf("     **     \n");
+//    printf("************\n");
+//    printf("************\n");
+//    printf("    *  *    \n");
+//    printf("    *  *    \n");
+//
+//    return 0;
+//}
+
+
+
