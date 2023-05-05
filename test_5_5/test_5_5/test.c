9ea9c0e6b769e87bfa4e0c7255a83069db55264f
tianhuo-tongren <1811334612@qq.com>
2023-05-05 15:02:35 +0800

    23_5_5 C语言代码提交

---
+++ test_5_5/test_5_5/test.c
@@ -0,0 +1,166 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//
+//int main()
+//{
+//    int a = 0xABCDEF;
+//    printf("%15d\n", a);
+//    return 0;
+//}
+//
+//
+//int main()
+//{
+//    int a = printf("Hello world!");
+//    printf("\n%d\n", a);
+//    return 0;
+//}
+//
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    scanf("%d %d %d", &a, &b, &c);
+//    printf("score1=%d,score2=%d,score3=%d", a, b, c);
+//    return 0;
+//}
+
+//
+//int main()
+//{
+//    int num = 0;
+//    float sco1 = 0;
+//    float sco2 = 0;
+//    float sco3 = 0;
+//    scanf("%d;%f,%f,%f", &num, &sco1, &sco2, &sco3);
+//    printf("The each subject score of No. %d is %.2f, %.2f, %.2f.", num, sco1, sco2, sco3);
+//
+//    return 0;
+//}
+//
+//int main()
+//{
+//	char ch = 0;
+//	scanf("%c", &ch);
+//	for (int i = 1; i <= 5; i++)
+//	{
+//		for (int j = 0; j < (5 - i); j++)
+//			printf(" ");
+//
+//		for (int k = 0; k < i; k++)
+//		{
+//			printf("%c", ch);
+//			if (k == 4)
+//				break;
+//			printf(" ");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//    int arr[12] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116 , 33 };
+//
+//    for (int i = 0; i < 12; i++)
+//        printf("%c", arr[i]);
+//    return 0;
+//}
+
+
+//int main()
+//{
+//	int y = 0;
+//	int m = 0;
+//	int d = 0;
+//	scanf("%4d%2d%2d", &y, &m, &d);
+//
+//	printf("year=%d\nmonth=%02d\ndate=%02d\n", y, m, d);
+//	return 0;
+//}
+//
+
+//
+//int main()
+//{
+//	int year = 0;
+//	int month = 0;
+//	int date = 0;
+//	scanf("%4d%2d%2d", &year, &month, &date);
+//
+//	printf("year=%d\nmonth=%02d\ndate=%02d\n", year, month, date);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("a=%d,b=%d", &a, &b);
+//
+//	int tmp = a;
+//	a = b;
+//	b = tmp;
+//
+//	printf("a=%d,b=%d\n", a, b);
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	char ch = 0;
+//	scanf("%c", &ch);
+//	printf("%d\n", ch);
+//	return 0;
+//}
+
+
+//
+//int main()
+//{
+//	int a = 40;
+//	int c = 212;
+//	int s = (-8 + 22) * a - 10 + c / 2;
+//	printf("%d\n", s);
+//
+//	return 0;
+//}
+
+
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d\n", a / b, a % b);
+//	return 0;
+//}
+
+
+//
+//void print(int n)
+//{
+//	//1234
+//	//123 4
+//	printf("%d", n % 10);
+//	if (n > 9)
+//		print(n / 10);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print(n);
+//
+//	return 0;
+//}
+
+
