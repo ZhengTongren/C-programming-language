41210cafab6c2b9768bf1bef9c6ca8a32a3ac551
tianhuo-tongren <1811334612@qq.com>
2023-05-06 23:40:36 +0800

    23_5_6 C语言代码提交

---
+++ test_5_6/test_5_6/test.c
@@ -0,0 +1,100 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &a, &b);
+//    a %= 100;
+//    b %= 100;
+//
+//    int s = a + b;
+//    s %= 100;
+//    printf("%d\n", s);
+//    return 0;
+//}
+
+
+//
+//int main()
+//{
+//	float a = 0;
+//	scanf("%f", &a);
+//
+//	int s = (int)a % 10;
+//	printf("%d", s);
+//	return 0;
+//}
+
+
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//
+//	long long t = n * 31560000;
+//	printf("%lld\n", t);
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int s = 0;
+//	scanf("%d", &s);
+//
+//	int h = s / 3600;
+//	int m = (s - h * 3600) / 60;
+//	s %= 60;
+//	printf("%d %d %d\n", h, m, s);
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	float s1 = 0;
+//	float s2 = 0;
+//	float s3 = 0;
+//	scanf("%f %f %f", &s1, &s2, &s3);
+//
+//	float sum = s1 + s2 + s3;
+//	float ave = sum / 3;
+//	printf("%.2f %.2f", sum, ave);
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int h = 0;
+//	int w = 0;
+//	scanf("%d %d", &w, &h);
+//	float bmi = w * 10000.0 / (h * h);
+//	printf("%.2f\n", bmi);
+//
+//	return 0;
+//}
+
+//
+//#include <stdio.h>
+//#include<math.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//
+//	float cir = a + b + c;
+//	float d = cir / 2.0;
+//	float area = sqrt(d * (d - a) * (d - b) * (d - c));
+//	printf("circumference=%.2f area=%.2f", cir, area);
+//	return 0;
+//}
+
+
