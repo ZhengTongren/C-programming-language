8aa50745f1d5bfaa55ab05de0215d889cf723491
tianhuo-tongren <1811334612@qq.com>
2023-05-17 23:43:50 +0800

    23_5_17 C语言代码提交

---
+++ test_5_17/test_5_17/test.c
@@ -0,0 +1,100 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("The size of short is %d bytes.\n", sizeof(short));
+//    printf("The size of int is %d bytes.\n", sizeof(int));
+//    printf("The size of long is %d bytes.\n", sizeof(long));
+//    printf("The size of long long is %d bytes.\n", sizeof(long long));
+//    return 0;
+//}
+ 
+ 
+
+//int main()
+//{
+//    int a = 1234;
+//    printf("%#o %#X\n", a, a);
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    int a = 0xABCDEF;
+//    printf("%15d\n", a);
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    int a = printf("Hello world!");
+//    printf("\n%d\n", a);
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    scanf("%d %d %d", &a, &b, &c);
+//    printf("score1=%d,score2=%d,score3=%d\n", a, b, c);
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    int num = 0;
+//    float a = 0.0f;
+//    float b = 0.0f;
+//    float c = 0.0f;
+//    scanf("%d;%f,%f,%f", &num, &a, &b, &c);
+//    printf("The each subject score of No. %d is %.2f, %.2f, %.2f.\n", num, a, b, c);
+//    return 0;
+//}
+
+
+//int main()
+//{
+//	char ch = 0;
+//	scanf("%c", &ch);
+//
+//	int i = 0;
+//	for (i = 1; i <= 5; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= 5 - i; j++)
+//			printf(" ");
+//		int k = 0;
+//		for (k = 1; k <= i; k++)
+//		{
+//			printf("%c", ch);
+//			if (k != 5)
+//				printf(" ");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    int arr[12] = { 73,32,99,97,110,32,100,111,32,105,116,33 };
+//
+//    int i = 0;
+//    for (i = 0; i < 12; i++)
+//        printf("%c", arr[i]);
+//
+//    return 0;
+//}
+
+
