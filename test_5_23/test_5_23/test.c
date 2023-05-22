6b92c265ed753ea6b86a707fffba481ffede8d68
tianhuo-tongren <1811334612@qq.com>
2023-05-23 00:08:48 +0800

    23_5_23 C语言代码提交

---
+++ test_5_23/test_5_23/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int c = 0;
+//    int m = 0;
+//    int e = 0;
+//    float ave = 0.0f;
+//    int count = 0;
+//    for (int i = 0; i < n; i++)
+//    {
+//        scanf("%d %d %d", &c, &m, &e);
+//        ave = (c + m + e) / 3.0;
+//        if (ave < 60)
+//            count++;
+//    }
+//    printf("%d\n", count);
+//    return 0;
+//}
+
+
