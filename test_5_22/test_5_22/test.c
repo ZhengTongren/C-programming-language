318e71eda77ee1de923585227ebb64c4b973e023
tianhuo-tongren <1811334612@qq.com>
2023-05-22 11:06:42 +0800

    23_5_22 C语言代码提交

---
+++ test_5_22/test_5_22/test.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//    int n = 0;
+//    int m = 0;
+//    scanf("%d %d", &n, &m);
+//    int arr1[10][10] = { 0 };
+//    int arr2[10][10] = { 0 };
+//    int i = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < m; j++)
+//        {
+//            scanf("%d", &arr1[i][j]);
+//        }
+//    }
+//    for (i = 0; i < n; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < m; j++)
+//        {
+//            scanf("%d", &arr2[i][j]);
+//        }
+//    }
+//    int flag = 1;
+//    for (i = 0; i < n; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < m; j++)
+//        {
+//            if (arr1[i][j] != arr2[i][j])
+//            {
+//                flag = 0;
+//                break;
+//            }
+//        }
+//    }
+//    if (1 == flag)
+//        printf("Yes\n");
+//    else
+//        printf("No\n");
+//    return 0;
+//}
+
+
