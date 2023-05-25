c682c640463ade14b65b3100bcd719107a4b917e
tianhuo-tongren <1811334612@qq.com>
2023-05-25 09:05:26 +0800

    23_5_25 C语言代码提交

---
+++ test_5_25/test_5_25/test.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//    long long a = 0;
+//    long long b = 0;
+//    scanf("%lld %lld", &a, &b);
+//    //24 6
+//    //4 0
+//    long long tmp = a % b;
+//    long long a1 = a;
+//    long long b1 = b;
+//    while (tmp)
+//    {
+//        a = b;
+//        b = tmp;
+//        tmp = a % b;
+//    }
+//    long long s = b + a1 * b1 / b;
+//    printf("%lld\n", s);
+//    return 0;
+//}
+
+
+//int Print(int n)
+//{
+//    if (n > 9)
+//    {
+//        return (n % 10) % 2 + 10 * Print(n / 10);
+//    }
+//    else 
+//    {
+//        return n % 2;
+//    }
+//}
+//
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int ret = Print(n);
+//    printf("%d\n", ret);
+//    return 0;
+//}
+
+
