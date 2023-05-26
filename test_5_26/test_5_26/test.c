58cb5bdfd3f66df28875172936791965eec2e5c8
tianhuo-tongren <1811334612@qq.com>
2023-05-26 11:12:29 +0800

    23_5_26 C语言代码提交

---
+++ test_5_26/test_5_26/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//void PrintN(int N)
+//{
+//    for (int i = 1; i <= N; i++)
+//        printf("%d\n", i);
+//}
+//
+//int main()
+//{
+//    int N = 0;
+//    scanf("%d", &N);
+//    PrintN(N);
+//    return 0;
+//}
+
+
