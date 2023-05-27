c42c2168de8ae581c4a86b7f61c11e8e0ba03e73
tianhuo-tongren <1811334612@qq.com>
2023-05-27 19:31:10 +0800

    23_5-27 C语言代码提交

--- test_5_27/test_5_27/test.c
+++ test_5_27/test_5_27/test.c
@@ -13,26 +13,52 @@ int My_strlen(char ch[])
     return i;
 }
 
-int main()
-{
-    char ch[100] = "i like beijing.";
-    
-    //scanf("%s", ch);
-    int len = My_strlen(ch);
+//int main()
+//{
+//    char ch[100] = "i like beijing.";
+//    
+//    //scanf("%s", ch);
+//    int len = My_strlen(ch);
+//
+//    //int len=strlen(ch);
+//    //printf("%d", len);
+//    
+//    for(int i=len-1;i>0;i--)
+//    {
+//        if(ch[i]==' ')
+//        {
+//            for(int j=i+1;ch[j]!='\0' && ch[j]!=' ';j++)
+//                printf("%c",ch[j]);
+//            printf("%c",ch[i]);
+//        }
+//    }
+//    if(ch[0]!=' ')
+//        printf("%c\n",ch[0]);
+//    return 0;
+//}
 
-    //int len=strlen(ch);
-    //printf("%d", len);
-    
-    for(int i=len-1;i>0;i--)
-    {
-        if(ch[i]==' ')
-        {
-            for(int j=i+1;ch[j]!='\0' && ch[j]!=' ';j++)
-                printf("%c",ch[j]);
-            printf("%c",ch[i]);
-        }
-    }
-    if(ch[0]!=' ')
-        printf("%c\n",ch[0]);
-    return 0;
-}
+
+//int main()
+//{
+//    char ch[100] = { 0 };
+//
+//    //scanf("%s", ch);
+//    gets(ch);
+//    int len = My_strlen(ch);
+//
+//    //int len=strlen(ch);
+//    //printf("%d", len);
+//
+//    for (int i = len - 1; i > 0; i--)
+//    {
+//        if (ch[i] == ' ')
+//        {
+//            for (int j = i + 1; ch[j] != '\0' && ch[j] != ' '; j++)
+//                printf("%c", ch[j]);
+//            printf("%c", ch[i]);
+//        }
+//    }
+//    if (ch[0] != ' ')
+//        printf("%c\n", ch[0]);
+//    return 0;
+//}
