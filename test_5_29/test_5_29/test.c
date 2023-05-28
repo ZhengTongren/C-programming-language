4d81905627a9953db8a92e71f7cbafaeb03b090f
tianhuo-tongren <1811334612@qq.com>
2023-05-29 01:04:53 +0800

    23_5_29 C语言代码提交

---
+++ test_5_29/test_5_29/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<limits.h>
+
+typedef float ElementType;
+#define MAX 10
+
+//ElementType Max(ElementType arr[], int n)
+//{
+//	ElementType max = INT_MIN;
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (arr[i] > max)
+//			max = arr[i];
+//	}
+//	return max;
+//}
+//
+//int main()
+//{
+//	ElementType arr[MAX] = { 0 };
+//	int n = 0;
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++)
+//		scanf("%f", &arr[i]);
+//	ElementType m = Max(arr, n);
+//	printf("%.2f\n", m);
+//	return 0;
+//}
+
+
