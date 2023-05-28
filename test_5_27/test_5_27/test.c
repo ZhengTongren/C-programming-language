02e2e5a5424e26ddd6ace2ffa59cefd83c756e0e
tianhuo-tongren <1811334612@qq.com>
2023-05-28 13:16:43 +0800

    23_5_28 C语言代码提交

--- test_5_27/test_5_27/test.c
+++ test_5_27/test_5_27/test.c
@@ -62,3 +62,17 @@ int My_strlen(char ch[])
 //        printf("%c\n", ch[0]);
 //    return 0;
 //}
+
+
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+    for (i = 0; i <= 12; i++)
+    {
+        arr[i] = 0;
+        printf("hello bit\n");
+    }
+    return 0;
+}
---
+++ test_5_28/test_5_28/test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//#define MAX 10
+//
+//double f(int n, double arr[], double x)
+//{
+//	double sum = 0;
+//	for (int i = 0; i <= n; i++)
+//	{
+//		if (0 == i)
+//			sum += arr[i];
+//		else
+//		{
+//			double xi = 1;
+//			for (int j = 1; j <= i; j++)
+//				xi *= x;
+//			sum += arr[i] * xi;
+//		}
+//	}
+//	return sum;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	double x = 0.0;
+//	scanf("%d %lf",&n, &x);
+//	double arr[MAX] = { 0.0 };
+//	for (int i = 0; i <= n; i++)
+//		scanf("%lf", &arr[i]);
+//	double y = f(n, arr, x);
+//	printf("%.1f\n", y);
+//	return 0;
+//}
+
+
