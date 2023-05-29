be4d0eccf64d552213527fc27a2d80c232a90d0c
tianhuo-tongren <1811334612@qq.com>
2023-05-30 00:27:26 +0800

    23_5_30 C语言代码提交

--- test_5_29/test_5_29/test.c
+++ test_5_29/test_5_29/test.c
@@ -30,3 +30,59 @@ typedef float ElementType;
 //}
 
 
+//int main()
+//{
+//	long long a = 0;
+//	long long b = 0;
+//	scanf("%lld %lld", &a, &b);
+//	long long s = 0;
+//	for (int i = 1;; i++)
+//	{
+//		if (i * a % b == 0)
+//		{
+//			s = i * a;
+//			break;
+//		}
+//	}
+//	printf("%lld\n", s);
+//	return 0;
+//}
+
+#include<string.h>
+
+void reverse(char* start, char* cur)
+{
+	while (start < cur)
+	{
+		int tmp = *start;
+		*start = *cur;
+		*cur = tmp;
+		start++;
+		cur--;
+	}
+}
+
+int main()
+{
+	char ch[101] = { 0 };
+	fgets(ch, 100, stdin);
+	int len = strlen(ch);
+
+	reverse(ch, ch + len - 1);
+
+	char* start = ch;
+	char* cur = start;
+	while (*cur)
+	{
+		while (*cur != ' ' && *cur != '\0')
+		{
+			cur++;
+		}
+		reverse(start, cur);
+		start = cur + 1;
+		if (*cur != '\0')
+			cur++;
+	}
+	printf("%s\n", ch);
+	return 0;
+}
