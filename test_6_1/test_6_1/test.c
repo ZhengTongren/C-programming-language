661da14063f7bc80d778c5aa6aa7b78b29fcf03d
tianhuo-tongren <1811334612@qq.com>
2023-06-01 14:23:05 +0800

    23_6_1 C语言代码提交

---
+++ test_6_1/test_6_1/test.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	char ch[10001] = { 0 };
+//	fgets(ch, 10000, stdin);
+//	int len = strlen(ch);
+//
+//	int left = 0;
+//	int right = len - 2;
+//
+//	while (left < right)
+//	{
+//		char tmp = ch[left];
+//		ch[left] = ch[right];
+//		ch[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//	printf("%s\n", ch);
+//
+//	return 0;
+//}
+
+
