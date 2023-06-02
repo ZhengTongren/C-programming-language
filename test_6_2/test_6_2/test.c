52200970141a8f31de9763cc9cee5e884151c6f7
tianhuo-tongren <1811334612@qq.com>
2023-06-02 17:57:24 +0800

    23_6_2 C语言代码提交

--- test_6_2/test_6_2/test.c
+++ test_6_2/test_6_2/test.c
@@ -1,3 +1,62 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 0x11223344;
+//	int* pa = &a;
+//	*pa = 0;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0x11223344;
+//	char* pa = &a;
+//	*pa = 0;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	char* pc = &a;
+//	printf("%p\n", pa);
+//	printf("%p\n", pc);
+//
+//	printf("%p\n", pa+1);
+//	printf("%p\n", pc+1);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0x11223344;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		*p = i;
+//		p++;
+//	}
+//
+//	
+//	return 0;
+//}
+
 
