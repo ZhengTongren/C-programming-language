c7b84006f9d5b5c8b8b7beb97c81b51bd1066f0d
tianhuo-tongren <1811334612@qq.com>
2023-06-03 16:30:55 +0800

    23_6_3 C语言代码提交

--- test_6_3/test_6_3/test.c
+++ test_6_3/test_6_3/test.c
@@ -105,3 +105,48 @@
 //}
 
 
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	int** ppa = &pa;
+//
+//	**ppa = 100;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "hello world";
+//
+//	char* parr[] = { arr1,arr2 };
+//	for (int i = 0; i < 2; i++)
+//		printf("%s\n", parr[i]);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//    int arr1[] = { 1,2,3,4,5 };
+//    int arr2[] = { 2,3,4,5,6 };
+//    int arr3[] = { 3,4,5,6,7 };
+//
+//    int* parr[] = { arr1,arr2,arr3 };
+//    for (int i = 0; i < 3; i++)
+//    {
+//        for (int j = 0; j < 5; j++)
+//        {
+//            printf("%d ", (parr[i])[j]);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+
