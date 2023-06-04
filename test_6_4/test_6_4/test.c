138b1ccad1c4d164e4b8ac4d972c252bcef568ea
tianhuo-tongren <1811334612@qq.com>
2023-06-04 13:26:41 +0800

    23_6_4 C语言代码提交

--- test_6_4/test_6_4/test.c
+++ test_6_4/test_6_4/test.c
@@ -75,17 +75,142 @@
 //}
 
 
-void reserve(int n)
-{
-	
-}
+//void reserve(int n)
+//{
+//	if (n > 0)
+//	{
+//		printf("%")
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	//int ret = reserve(n);
+//	reserve(n);
+//	//printf("%d\n", ret);
+//	return 0;
+//}
 
-int main()
-{
-	int n = 0;
-	scanf("%d", &n);
-	//int ret = reserve(n);
-	reserve(n);
-	//printf("%d\n", ret);
-	return 0;
-}
+
+//int main()
+//{
+//	int n = 18;
+//	printf("%x", n);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	//for (i = 0; i <= 153; i++)
+//	scanf("%d", &i);
+//	printf("%x ", i);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("------------------------------------\n");
+//	printf("Province      Area(km2)   Pop.(10K)\n");
+//	printf("------------------------------------\n");
+//	printf("Anhui         139600.00   6461.00\n");
+//	printf("Beijing        16410.54   1180.70\n");
+//	printf("Chongqing      82400.00   3144.23\n");
+//	printf("Shanghai        6340.50   1360.26\n");
+//	printf("Zhejiang      101800.00   4894.00\n");
+//	printf("------------------------------------\n");
+//
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	char ch = 0;
+//	float a1 = 0.0f;
+//	float a2 = 0.0f;
+//	scanf("%f %d %c %f", &a1, &a, &ch, &a2);
+//	printf("%c %d %.2f %.2f\n", ch, a, a1, a2);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int h = 0;
+//	int m = 0;
+//	scanf("%d:%d", &h, &m);
+//	if (h >= 12)
+//	{
+//		h %= 12;
+//		printf("%d:%d PM\n", h, m);
+//	}
+//	else
+//	{
+//		printf("%d:%d AM\n", h, m);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int v = 0;
+//	scanf("%d", &v);
+//	if (v > 60)
+//		printf("Speed: %d - Speeding\n", v);
+//	else
+//		printf("Speed: %d - OK\n", v);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	if (a == b)
+//		printf("C\n");
+//	else if (a == c)
+//		printf("B\n");
+//	else
+//		printf("A\n");
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int y = 0;
+//	long long h = 0;
+//	scanf("%d %lld", &y, &h);
+//	double p = 0;
+//
+//	if (y >= 5)
+//	{
+//		if (h > 40)
+//			p = 2000.0 + (h - 40) * 50 * 1.5;
+//		else
+//			p = h * 50.0;
+//	}
+//	else
+//	{
+//		if (h > 40)
+//			p = 1200.0 + (h - 40) * 30 * 1.5;
+//		else
+//			p = h * 30.0;
+//	}
+//	printf("%.2f\n", p);
+//	return 0;
+//}
+
+
