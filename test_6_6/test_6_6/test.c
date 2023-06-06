b41a2fffad37000bad9dd90fb5cb4f3080a90cf5
tianhuo-tongren <1811334612@qq.com>
2023-06-06 17:16:53 +0800

    23_6_6 C语言代码提交

--- test_6_6/test_6_6/test.c
+++ test_6_6/test_6_6/test.c
@@ -1,1 +1,53 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int n = 0;
+//	int u = 0;
+//	int d = 0;
+//	scanf("%d %d %d", &n, &u, &d);
+//	int t = 0;
+//	int s = 0;
+//	for (t = 1; t; t++)
+//	{
+//		if (t % 2)
+//		{
+//			s += u;
+//		}
+//		else
+//		{
+//			s -= d;
+//		}
+//		if (s >= n)
+//			break;
+//
+//	}
+//	printf("%d\n", t);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a3 = 0;
+//	int a2 = 0;
+//	int a1 = 0;
+//	int a0 = 0;
+//	scanf("%d %d %d %d", &a3, &a2, &a1, &a0);
+//	double a = 0;
+//	double b = 0;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int f = 0;
+//	int y = 0;
+//
+//	return 0;
+//}
+
+
