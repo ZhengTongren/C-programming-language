356ac54dda4f480602391caf4e7f45708e7b54f6
tianhuo-tongren <1811334612@qq.com>
2023-06-08 14:16:39 +0800

    23_6_8 C语言代码提交

---
+++ test_6_8/test_6_8/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int t = 0;
+//	scanf("%d", &t);
+//	int s1 = 0;
+//	int s2 = 0;
+//
+//	int flag = 1;
+//	int t1 = 0;
+//	for (int i = 0; i < t / 10 + 1; i++)
+//	{
+//		for (int j = 1; j <= 10 && i * 10 + j <= t; j++)
+//		{
+//			s1 += 3;
+//			if (1 == flag)
+//				s2 += 9;
+//		}
+//		if (s2 > s1)
+//		{
+//			flag = 0;
+//			t1 = i;
+//		}
+//		if (i - t1 == 3)
+//		{
+//			flag = 0;
+//		}
+//	}
+//	if (s1 > s2)
+//		printf("@_@ %d\n", s1);
+//	else if (s1 < s2)
+//		printf("^_^ %d\n", s2);
+//	else
+//		printf("-_- %d\n", s1);
+//	return 0;
+//}
+
+
