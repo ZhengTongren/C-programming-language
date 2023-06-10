828a29544804dcf1631e9c5703fdbeaff0b34327
tianhuo-tongren <1811334612@qq.com>
2023-06-10 22:42:04 +0800

    23_6_10 C语言代码提交

---
+++ test_6_10/test_6_10/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//void bubble_sort(int arr[], int n, int k)
+//{
+//	for (int i = 0; i < k; i++)
+//	{
+//		for (int j = 0; j < n - i - 1; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	int arr[100] = { 0 };
+//	scanf("%d %d", &n, &k);
+//	for (int i = 0; i < n; i++)
+//		scanf("%d", &arr[i]);
+//
+//	bubble_sort(arr, n, k);
+//
+//	for (int i = 0; i < n; i++)
+//		printf("%d ", arr[i]);
+//	return 0;
+//}
+
+
