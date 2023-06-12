7c6e1ed19d73f6333f82d4e4a6243e4ae5384697
tianhuo-tongren <1811334612@qq.com>
2023-06-12 16:00:17 +0800

    23_6_12 C语言代码提交

---
+++ test_6_12/test_6_12/test.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	char ch1[81] = { 0 };
+//	char ch2[81] = { 0 };
+//	gets(ch1);
+//	gets(ch2);
+//
+//	int len1 = strlen(ch1);
+//	int len2 = strlen(ch2);
+//	//cat 3
+//	//
+//
+//	for (int i = 0; i < len1; i++)
+//	{
+//		if (ch1[i] == ch2[0])
+//		{
+//			int flag = 1;
+//			for (int j = 1; j < len2; j++)
+//			{
+//				if (ch1[i + j] != ch2[j])
+//				{
+//					flag = 0;
+//					break;
+//				}
+//			}
+//			if (1 == flag)
+//			{
+//				for (int j = 0; j < len2; j++)
+//				{
+//					ch1[i + j] = 0;
+//
+//				}
+//			}
+//		}
+//	}
+//	for (int i = 0; i < len1; i++)
+//	{
+//		if (ch1[i])
+//		{
+//			printf("%c", ch1[i]);
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	char ch1[100][10] = { 0 };
+//	scanf("%d", &n);
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%s", &(ch1[i]));
+//	}
+//	for (int i = 0; i < k; i++)
+//	{
+//		for (int j = 0; j < n - i - 1; j++)
+//		{
+//			if(strlen(&ch[j])<strlen)
+//		}
+//	}
+//	return 0;
+//}
+
+
