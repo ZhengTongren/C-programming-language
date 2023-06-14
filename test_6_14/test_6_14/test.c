d288ac36ea77f94597d5134957d691644e23ecaf
tianhuo-tongren <1811334612@qq.com>
2023-06-14 17:47:00 +0800

    23_6_14 C语言代码提交

---
+++ test_6_14/test_6_14/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+
+//void Left_shift(char ch1[], int len, int n)
+//{
+//	for (int j = 0; j < n; j++)
+//	{
+//		for (int i = 0; i < len - 1; i++)
+//		{
+//			char tmp = ch1[i];
+//			ch1[i] = ch1[i + 1];
+//			ch1[i + 1] = tmp;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	char ch1[101] = { 0 };
+//	gets(ch1);
+//	int n = 0;
+//	scanf("%d", &n);
+//	int len = strlen(ch1);
+//
+//	Left_shift(ch1, len, n);
+//
+//	for (int i = 0; i < len; i++)
+//		printf("%c", ch1[i]);
+//	return 0;
+//}
+
+
