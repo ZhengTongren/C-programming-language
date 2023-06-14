c1a1da2111ae6438c5c6b3bb9d742a418f35f24a
tianhuo-tongren <1811334612@qq.com>
2023-06-14 18:26:01 +0800

    23_6_14 C语言代码提交

--- test_6_14/test_6_14/test.c
+++ test_6_14/test_6_14/test.c
@@ -32,3 +32,79 @@
 //}
 
 
+
+
+
+//void reverse(char ch[], int len)
+//{
+//	int left = 0;
+//	int right = len - 1;
+//
+//	while (left < right)
+//	{
+//		char tmp = ch[left];
+//		ch[left] = ch[right];
+//		ch[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+////void Left_shift(char ch[], int len)
+////{
+////	for (int i = 0; i < len; i++)
+////	{
+////		char tmp = ch[i];
+////		ch[i] = ch[i + 1];
+////		ch[i + 1] = tmp;
+////	}
+////}
+//
+//void Delete(char ch[], int len)
+//{
+//	for (int i = 0; i < len; i++)
+//	{
+//		if (ch[i] == ' ')
+//		{
+//			if (ch[i + 1] == ' ')
+//			{
+//				ch[i] = '0';
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	char ch[1000] = { 0 };
+//	gets(ch);
+//
+//	int len = strlen(ch);
+//	
+//	Delete(ch, len);
+//
+//	reverse(ch, len);
+//
+//	char* start = ch;
+//	char* end = ch;
+//	while (*end != '\0')
+//	{
+//		while (*end != ' ' && *end != '\0')
+//			end++;
+//
+//		reverse(start, end - start);
+//		start = end + 1;
+//
+//		if (*start != '\0')
+//			end = start;
+//	}
+//
+//	for (int i = 0; i < len; i++)
+//	{
+//		if (ch[i] != '0')
+//			printf("%c", ch[i]);
+//	}
+//	return 0;
+//}
+
+
