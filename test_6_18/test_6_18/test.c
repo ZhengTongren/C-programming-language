a6ee43550856d9104d5ce206a2c40714baafd778
tianhuo-tongren <1811334612@qq.com>
2023-06-18 14:43:45 +0800

    23_6_18 C语言代码提交

---
+++ test_6_18/test_6_18/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	char cal[5] = { '+','-','*,'/'};
+	double a1 = 0, b1 = 0;
+	double a2 = 0, b2 = 0;
+	
+	scanf("%lf %lf %lf %lf", &a1, &b1, &a2, &b2);
+
+	
+	return 0;
+}
