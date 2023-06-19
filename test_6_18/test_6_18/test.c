8b38d03bfa9d532001bd5f794e2b9ff52c24f684
tianhuo-tongren <1811334612@qq.com>
2023-06-19 19:55:38 +0800

    23_6_19 NULL

--- test_6_18/test_6_18/test.c
+++ test_6_18/test_6_18/test.c
@@ -2,14 +2,16 @@
 
 #include<stdio.h>
 
-int main()
-{
-	char cal[5] = { '+','-','*,'/'};
-	double a1 = 0, b1 = 0;
-	double a2 = 0, b2 = 0;
-	
-	scanf("%lf %lf %lf %lf", &a1, &b1, &a2, &b2);
+//int main()
+//{
+//	char cal[5] = { '+','-','*,'/'};
+//	double a1 = 0, b1 = 0;
+//	double a2 = 0, b2 = 0;
+//	
+//	scanf("%lf %lf %lf %lf", &a1, &b1, &a2, &b2);
+//
+//	
+//	return 0;
+//}
 
-	
-	return 0;
-}
+
