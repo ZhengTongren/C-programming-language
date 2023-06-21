c288cf0b7c59a77514656356a15cbe64058281be
tianhuo-tongren <1811334612@qq.com>
2023-06-21 16:56:00 +0800

    23_6_21 C语言代码提交

---
+++ test_6_21/test_6_21/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//void PrintN(int N)
+//{
+//    for (int i = 1; i <= N; i++)
+//    {
+//        printf("%d\n", i);
+//    }
+//}
+//
+//int main()
+//{
+//    int N;
+//
+//    scanf("%d", &N);
+//    PrintN(N);
+//
+//    return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//#define MAXN 10
+//
+//double f(int n, double a[], double x)
+//{
+//    double sum = 0;
+//    for (int i = 0; i <= n; i++)
+//    {
+//        double x1 = 1;
+//        if (i > 0)
+//        {
+//            for (int j = 1; j <= i; j++)
+//            {
+//                x1 *= x;
+//            }
+//        }
+//        sum += a[i] * x1;
+//    }
+//    return sum;
+//}
+//
+//int main()
+//{
+//    int n, i;
+//    double a[MAXN], x;
+//
+//    scanf("%d %lf", &n, &x);
+//    for (i = 0; i <= n; i++)
+//        scanf("%lf", &a[i]);
+//    printf("%.1f\n", f(n, a, x));
+//    return 0;
+//}
+
+
