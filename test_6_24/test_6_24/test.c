ca9704e3fdb18b876178d3e8b53323e14d2c7942
tianhuo-tongren <1811334612@qq.com>
2023-06-24 17:37:28 +0800

    23_6_24 C语言代码提交

---
+++ test_6_24/test_6_24/test.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//#define MAXN 10
+//
+//int Sum(int List[], int N)
+//{
+//    int sum = 0;
+//    for (int i = 0; i < N; i++)
+//    {
+//        sum += List[i];
+//    }
+//    return sum;
+//}
+//
+//int main()
+//{
+//    int List[MAXN], N, i;
+//
+//    scanf("%d", &N);
+//    for (i = 0; i < N; i++)
+//        scanf("%d", &List[i]);
+//    printf("%d\n", Sum(List, N));
+//
+//    return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//#define MAXN 10
+//typedef float ElementType;
+//
+//ElementType Average(ElementType S[], int N)
+//{
+//    ElementType ave = 0.0f;
+//    for (int i = 0; i < N; i++)
+//    {
+//        ave += S[i];
+//    }
+//    ave /= N;
+//    return ave;
+//}
+//
+//int main()
+//{
+//    ElementType S[MAXN];
+//    int N, i;
+//
+//    scanf("%d", &N);
+//    for (i = 0; i < N; i++)
+//        scanf("%f", &S[i]);
+//    printf("%.2f\n", Average(S, N));
+//
+//    return 0;
+//}
+
+
