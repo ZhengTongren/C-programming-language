57d17746245182caf8e2fdfe209ef488105fdc7a
tianhuo-tongren <1811334612@qq.com>
2023-07-14 08:10:33 +0800

    23_7_13 C语言代码提交

--- test_7_13/test_7_13/test.c
+++ test_7_13/test_7_13/test.c
@@ -21,4 +21,85 @@
 //	return 0;
 //}
 
+//void GetMemory(char* p)
+//{
+//	p = (char*)malloc(100);
+//}
+//void Test(void)
+//{
+//	char* str = NULL;
+//	GetMemory(str);
+//	strcpy(str, "hello world");
+//	printf(str);
+//}
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+
+//char* GetMemory(void)
+//{
+//	char p[] = "hello world";
+//	return p;
+//}
+//void Test(void)
+//{
+//	char* str = NULL;
+//	str = GetMemory();
+//	printf(str);
+//}
+//
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+
+//void GetMemory(char** p, int num)
+//{
+//	*p = (char*)malloc(num);
+//}
+//void Test(void)
+//{
+//	char* str = NULL;
+//	GetMemory(&str, 100);
+//	strcpy(str, "hello");
+//	printf(str);
+//	// Õ∑≈
+//	free(str);
+//	str = NULL;
+//}
+//
+//
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+
+//void Test(void)
+//{
+//	char* str = (char*)malloc(100);
+//	strcpy(str, "hello");
+//	free(str);
+//	//
+//	str = NULL;
+//
+//	if (str != NULL)
+//	{
+//		strcpy(str, "world");
+//		printf(str);
+//	}
+//}
+//
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
 
