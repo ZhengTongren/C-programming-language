f1d8367cf9c3c54f5569ad77bf4d76357ac22731
tianhuo-tongren <1811334612@qq.com>
2023-07-15 09:01:31 +0800

    23_7_15 C语言代码提交

---
+++ test_7_15/test_7_15/test.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//void GetMemory(char* p)
+//{
+//	p = (char*)malloc(100);
+//}
+//void Test(void)
+//{
+//	char* str = NULL;
+//	GetMemory(str);
+//	strcpy(str, "hello world");
+//	printf(str);
+//}
+
+
+//char* GetMemory(void)
+//{
+//	char p[] = "hello world";
+//	return p;
+//}
+//
+//void Test(void)
+//{
+//	char* str = NULL;
+//	str = GetMemory();
+//	printf(str);
+//}
+
+
