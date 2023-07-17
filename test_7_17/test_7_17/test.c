052c04ec9cb72ad5d95a60b85d5bee9696548758
tianhuo-tongren <1811334612@qq.com>
2023-07-17 12:17:06 +0800

    23_7_17 C语言代码提交

--- test_7_17/test_7_17/test.c
+++ test_7_17/test_7_17/test.c
@@ -1,2 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+//struct S
+//{
+//	int a;
+//	int arr[0];
+//};
+//
+//int main()
+//{
+//	struct S s;
+//	return 0;
+//}
+
+
+//struct S
+//{
+//	int a;
+//	int arr[];
+//};
+//
+//int main()
+//{
+//	struct S s;
+//	return 0;
+//}
+
+
