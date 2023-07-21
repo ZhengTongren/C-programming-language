a8c61cc5a25c89a35eb2dc05a805f02ac8007f4a
tianhuo-tongren <1811334612@qq.com>
2023-07-21 17:35:48 +0800

    23_7_21 C语言代码提交

---
+++ test_7_21/test_7_21/test.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//
+//	//5 1 2 3 4
+//	//1 3 5 7 9 11 
+//	long long mat_before = 0;
+//	if (n > 1)
+//		mat_before = (n - 1) * n / 2;
+//	else
+//		mat_before = n;
+//
+//	int count = 1;
+//	for (int i = 1; i < mat_before; i++)
+//	{
+//
+//	}
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int CountOdd(int n)
+//{
+//	int returnX = 1;
+//	for (int i = 1; i < n; i++)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			returnX += 2;
+//		}
+//	}
+//	return returnX;
+//}
+//
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//
+//	int x = CountOdd(n);
+//
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d", x);
+//		x += 2;
+//		if (i < n - 1)
+//			printf("+");
+//	}
+//	return 0;
+// }
+
+
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int a1 = 2;
+//	int an = 2;
+//	for (int i = 0; i < n - 1; i++)
+//	{
+//		an += 3;
+//	}
+//	long long sum = n * (a1 + an) / 2;
+//	printf("%lld\n", sum);
+//
+//	return 0;
+//}
+
+
