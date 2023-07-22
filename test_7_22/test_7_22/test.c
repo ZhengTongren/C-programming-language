035f178f061921005f741893417ad3cdd9e0564d
tianhuo-tongren <1811334612@qq.com>
2023-07-22 17:11:13 +0800

    23_7_22 C语言代码提交

---
+++ test_7_22/test_7_22/test.c
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+
+
+//int fun(char ch[10], int a)
+//{
+//	;
+//}
+//
+//int main()
+//{
+//	char ch[10] = { 0 };
+//	int a = 0;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	int arr[2000] = { 0 };
+//
+//	int sum = 0;
+//	float ave = 0.0f;
+//	int count1 = 0;
+//	int count2 = 0;
+//
+//	while (scanf("%d", &n) == 1)
+//	{
+//		for (int i = 0; i < n; i++)
+//		{
+//			scanf("%d", &arr[i]);
+//			if (arr[i] < 0)
+//			{
+//				count1++;
+//			}
+//			else if (arr[i] > 0)
+//			{
+//				sum += arr[i];
+//				count2++;
+//			}
+//		}
+//		if (sum)
+//			ave = sum * 1.0 / count2;
+//		
+//		printf("%d %.1f\n", count1, ave);
+//	}
+//	return 0;
+//}
+
+
+//int minNumberInRotateArray(int* nums, int numsLen) {
+//    // write code here
+//    int tmp = nums[0];
+//    //numsLen = sizeof(nums)/sizeof(nums[0]);
+//
+//    for (int i = 1; i < numsLen; i++)
+//    {
+//        if (tmp > nums[i])
+//        {
+//            tmp = nums[i];
+//        }
+//    }
+//    return tmp;
+//}
+
+
