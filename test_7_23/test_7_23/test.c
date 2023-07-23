6704691e8944764e71a41c522d2e87717307ba97
tianhuo-tongren <1811334612@qq.com>
2023-07-23 18:06:09 +0800

    23_7_23

---
+++ test_7_23/test_7_23/test.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//
+//void Test(int N)
+//{
+//	for (size_t i = 0; i < length; i++)
+//	{
+//
+//	}
+//}
+//
+//int main()
+//{
+//	return 0;
+//}
+
+
+//int main()
+//{
+//
+//	return 0;
+//}
+
+//long long Fib(int n)
+//{
+//
+//}
+//
+//
+//int main()
+//{
+//	long long x = Fib(50);
+//	printf("%lld\n", x);
+//	return 0;
+//}
+
+
+//int returnX(int* arr, int n)
+//{
+//	int x = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		x ^= i;
+//	}
+//	for(int i=0;i<n;i++)
+//}
+//
+//
+//int main()
+//{
+//	int arr[10] = { 0,1,2,3,5,6,7,8,9 };
+//	int x = returnX(arr, 10);
+//
+//	printf("%d\n", x);
+//	return 0;
+//}
+
