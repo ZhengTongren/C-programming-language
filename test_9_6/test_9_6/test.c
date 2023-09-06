8ec7df0c0065c12283fc3e9b3a628a311471a5ec
tianhuo-tongren <1811334612@qq.com>
2023-09-06 17:27:52 +0800

    23_9_6

---
+++ test_9_6/test_9_6/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char ch[200] = "HVOCZHVODXDVINVMZGDFZAMZIXCHZIRCVOZQZMTJPNVTOCZHOCZT OMVINGVOZ DIOJ OCZDM JRI GVIBPVBZVIY AJMOCRDOC DO DN NJHZOCDIB ZIODMZGT YDAAZMZIOBJZOCZ";
+//	int len = strlen(ch);
+//
+//	int count = 0;
+//	for (int i = 0; i < len; i++)
+//	{
+//		if (ch[i] == 'Z')
+//			count++;
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+
+
